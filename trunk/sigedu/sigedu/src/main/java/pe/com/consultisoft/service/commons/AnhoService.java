40f46edfda10e0f315d41565cf82751dea9151d3
w.custodiop <w.custodiop@e6c0c3b3-bb10-bf7e-bd56-9e849d7a7ebc>
2015-02-26 20:45:03 +0000

    Archivos han sido agregados al repo.

---
+++ trunk/sigedu/sigedu/src/main/java/pe/com/consultisoft/service/commons/AnhoService.java
@@ -0,0 +1,10 @@
+package pe.com.consultisoft.service.commons;
+
+import java.util.List;
+
+import pe.com.consultisoft.model.Anho;
+
+public interface AnhoService {
+	public List<Anho> listAnhos();
+
+}
---
+++ trunk/sigedu/sigedu/src/main/java/pe/com/consultisoft/service/commons/GeneroService.java
@@ -0,0 +1,10 @@
+package pe.com.consultisoft.service.commons;
+
+import java.util.List;
+
+import pe.com.consultisoft.model.Parametro;
+
+public interface GeneroService {
+	public List<Parametro> listGeneros();
+
+}
